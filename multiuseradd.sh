ed401c8c8622af616b60f70f9f93a438b1081863
Jesús Hernández Gormaz <jhg.jesus@gmail.com>
2012-01-10 03:40:20 +0000

    new script multiuseradd.sh and read file CSV line to line and split user and password.

---
+++ multiuseradd.sh
@@ -0,0 +1,29 @@
+##############################################################################
+##                                                                          ##
+## Multiuseradd is a script for linux that add news users from csv file.    ##
+##                                                                          ##
+## multiuseradd.sh (C) 2012 Jesús Hernández Gormaz.                         ##
+##                                                                          ##
+##   This program is free software; you can redistribute it and/or          ##
+##     modify it under the terms of the GNU General Public License as       ##
+##     published by the Free Software Foundation; either version 3, or      ##
+##     (at your option) any later version.                                  ##
+##     This program is distributed in the hope that it will be useful,      ##
+##     but WITHOUT ANY WARRANTY; without even the implied warranty of       ##
+##     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the         ##
+##     GNU General Public License for more details.                         ##
+##     You should have received a copy of the GNU General Public License    ##
+##     along with this program; if not, write to the Free Software          ##
+##     Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.            ##
+##                                                                          ##
+##############################################################################
+
+# Read file line to line and split user and password
+#  IFS is variable of Internal Field Separator, and the internal command read
+#  will read and split line to line while not end file.
+while IFS="," read user pass
+do
+  # Create new user
+  #useradd $user
+  echo -e "$user and $pass \n"
+done < $1
