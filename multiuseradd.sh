361fc437f9baac552d4a95d7bcf1b80de7beaa00
Jesús Hernández Gormaz <jhg.jesus@gmail.com>
2012-01-12 01:44:10 +0000

    Add --version parameter and write more help.

--- multiuseradd.sh
+++ multiuseradd.sh
@@ -19,11 +19,33 @@
 ##############################################################################
 
 # Help
-if [ $# -lt 2 ]
+if [ $# -lt 2 ] || [ $1 = "--help" ]
 then
   cat <<HELP
 Multiuseradd (C) 2012 Jesús Hernández Gormaz
+  This program is free software; you can redistribute it and/or
+    modify it under the terms of the GNU General Public License as
+    published by the Free Software Foundation; either version 3, or
+    (at your option) any later version.
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+    GNU General Public License for more details.
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+
+Multiuseradd is a script for linux that add news users from csv file.
 Only parameter of this script is the CSV file with users and passwords.
+HELP
+  exit
+fi
+
+# Help
+if [ $1 = "--version" ]
+then
+  cat <<HELP
+Multiuseradd 0.0.0
 HELP
   exit
 fi
